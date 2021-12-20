9482143b9a94a43a97ec2336ab1e07bcc8f74553
swy-326 <sungwon.yoon@usp.br>
2021-12-20 02:09:10 -0300

    System.exit(0) e (1)

--- client/src/main/java/br/usp/each/typerace/client/Client.java
+++ client/src/main/java/br/usp/each/typerace/client/Client.java
@@ -21,9 +21,7 @@ public class Client extends WebSocketClient {
 
     @Override
     public void onMessage(String message) {
-
         System.out.println("Servidor: " + message);
-
     }
 
     @Override
@@ -36,15 +34,17 @@ public class Client extends WebSocketClient {
     @Override
     public void onClose(int code, String reason, boolean remote) {
         System.out.println("closed with exit code " + code + " additional info: " + reason);
+        System.exit(0);
     }
-
 
     @Override
     public void onError(Exception ex) {
         System.err.println("an error occurred:" + ex);
+        System.exit(1);
     }
 
     public int getId() {
         return id;
     }
+
 }
