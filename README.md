f0efd00803044f4df340526fc76534e5184a81b6
gabij <gabi.jospin@gmail.com>
2021-12-21 11:46:56 -0300

    Reader class

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 Repositório para o EP2 de Redes de Computadores, EACH-USP - 2021/2
 
 # Integrantes
-* Integrante 1 - 1111111
-* Integrante 2 - 2222222
-* Integrante 3 - 3333333
-* Integrante 4 - 4444444
+* Maria Eduarda Rodrigues Garcia - 1111111
+* Gabriel Medeiros Jospin - 11796020
 
 ## Pré-requisitos
 * JDK 11 ou maior (testado com a JDK11 OpenJDK)
 * Gradle (incluso no repositório, não é necessário instalá-lo)
+* ~~Baidu Antivirus~~
 
 ### Rodando
 Para rodar o servidor
---
+++ server/src/main/java/br/usp/each/typerace/database/Reader.java
@@ -0,0 +1,75 @@
+package br.usp.each.typerace.database;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class Reader {
+
+    private String path;
+    private String fullText;
+    private Set<String> utilDB;
+
+    public Reader(String path, String fullText, Set<String> utilDB) {
+        this.path = path;
+        this.fullText = fullText;
+        this.utilDB = utilDB;
+    }
+
+    public void gerateFullText(){
+        File file = new File(path);
+        try {
+            PDDocument document = Loader.loadPDF(file);
+            AccessPermission ap = document.getCurrentAccessPermission();
+
+            if(!ap.canExtractContent())
+                throw new IOException("You do not have permission to extract text");
+
+            PDFTextStripper tracy = new PDFTextStripper(); //HIMYM S01E09
+            tracy.setSortByPosition(true);
+            tracy.setStartPage(0);
+            tracy.setEndPage(document.getNumberOfPages() -1);
+            this.fullText = tracy.getText(document);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void gerateUtilDB(){
+        List<String> texts = List.of(this.fullText.split(" "));
+        this.utilDB.addAll(texts);
+    }
+
+
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getFullText() {
+        return fullText;
+    }
+
+    public void setFullText(String fullText) {
+        this.fullText = fullText;
+    }
+
+    public Set<String> getUtilDB() {
+        return utilDB;
+    }
+
+    public void setUtilDB(Set<String> utilDB) {
+        this.utilDB = utilDB;
+    }
+}
